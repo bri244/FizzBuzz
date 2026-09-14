0b4cfba00f24bf5f77040bc33f7d1601210aac0f
briw24 <briannacandy2442@gmail.com>
2026-09-14 16:01:00 -0400

    finished extra task 1.1 :multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -6,19 +6,26 @@ public class Multiples {
         int a = 3;
         int b = 5;
         System.out.println(multiples(n, a, b));
+        System.out.println(multiples());
     }
 
+    static int multiples() {
+        int three_multi = 999/3;
+        int five_multi = 999/5;
+        int both_multi = 999/15;
+        return three_multi + five_multi - both_multi;
+    }
     static int multiples(int n, int a, int b) {
-        int a_multi = n/a;
+        int a_multi = (n-1)/a;
         if (a == b) {
 
             return a_multi;
 
         } else {
 
-            int b_multi = n/b;
-            int both_multi = n/(a*b);
-            return a_multi + b_multi - both_multi - 1;
+            int b_multi = (n-1)/b;
+            int both_multi = (n-1)/(a*b);
+            return a_multi + b_multi - both_multi;
 
         }
 
